5c55a7c1095a2ae19bec13575d85c37d115eaeb1
Omagra88915 <agrawalom840@gmail.com>
2024-10-08 14:02:17 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,10 +1,37 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+   var romanToInt = function(s) {
+    
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0; 
+
+    
+    for (let i = 0; i < s.length; i++) {
+        let currentVal = romanMap[s[i]]; 
+        let nextVal = romanMap[s[i + 1]]; 
+
+    
+        if (currentVal < nextVal) {
+            total -= currentVal;
+        } 
+        
+        else {
+            total += currentVal;
+        }
+    }
+
+    return total;
+};
+
+
 
 
 
