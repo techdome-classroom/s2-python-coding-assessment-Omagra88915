5aa71e8a1cabd6dc942e5e0730403cadbaa54ad3
Omagra88915 <agrawalom840@gmail.com>
2024-10-08 14:01:02 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,27 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+    var isValid = function(s) {
+    let stack = []; 
+
+    for (let char of s) {
+        
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } 
+        
+        else {
+            let top = stack.pop();
+            if ((char === ')' && top !== '(') ||
+                (char === '}' && top !== '{') ||
+                (char === ']' && top !== '[')) {
+                return false;
+            }
+        }
+    }
+
+    
+    return stack.length === 0;
+};
+
 
 
 
