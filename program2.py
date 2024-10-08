9413d846f03d1e04f446c8753c7511d12de8632f
Omagra88915 <agrawalom840@gmail.com>
2024-10-08 14:07:50 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,7 +1,7 @@
 class Solution(object):
-   var romanToInt = function(s) {
-    
-    const romanMap = {
+   def romanToInt(s):
+    # Map of Roman numerals and their corresponding integer values
+    roman_map = {
         'I': 1,
         'V': 5,
         'X': 10,
@@ -9,29 +9,20 @@ class Solution(object):
         'C': 100,
         'D': 500,
         'M': 1000
-    };
-
-    let total = 0; 
-
-    
-    for (let i = 0; i < s.length; i++) {
-        let currentVal = romanMap[s[i]]; 
-        let nextVal = romanMap[s[i + 1]]; 
-
-    
-        if (currentVal < nextVal) {
-            total -= currentVal;
-        } 
-        
-        else {
-            total += currentVal;
-        }
     }
 
-    return total;
-};
+    total = 0  # To store the final result
 
+    # Loop through the string
+    for i in range(len(s)):
+        current_val = roman_map[s[i]]  # Value of current Roman numeral
+        next_val = roman_map[s[i + 1]] if i + 1 < len(s) else 0  # Value of the next Roman numeral
 
+        # If the current value is less than the next value, subtract it
+        if current_val < next_val:
+            total -= current_val
+        # Otherwise, add it
+        else:
+            total += current_val
 
-
-
+    return total
